a2b5d526711d4fb44a692349590c64698a12acab
Guigxs <guigxs19@gmail.com>
2021-05-04 18:51:28 +0200

    create new route

--- Backend/src/Controller/WardrobeController.php
+++ Backend/src/Controller/WardrobeController.php
@@ -30,24 +30,37 @@ class WardrobeController extends AbstractController
         return $this->json($wardrobe, 201, ["Access-Control-Allow-Origin" => "*", "Access-Control-Allow-Headers" => "*"], ["groups"=>["show_wardrobe"]]);
     }
 
-    public function removeWardrobe(int $id, Request $request){
+    public function removeWardrobe($id, Request $request){
         if ($request->isMethod('OPTIONS')) {
             return $this->json([], 200, ["Access-Control-Allow-Origin" => "*", "Access-Control-Allow-Headers" => "*", "Access-Control-Allow-Methods" => "*"]);
         }
 
         $entityManager = $this->getDoctrine()->getManager();
-        $wardrobe = $this->getDoctrine()->getRepository(Wardrobe::class)->find($id);
-        
-        if ($wardrobe == null){
-            return $this->json([
-                "error"=>"Wardrobe not found"
-                ], 400, ["Access-Control-Allow-Origin" => "*", "Access-Control-Allow-Headers" => "*"], []);
-        }
+        if ($id == "all"){
+            $wardrobes = $this->getDoctrine()->getRepository(Wardrobe::class)->findAll();
+            
+            foreach ($wardrobes as $entity) {
+                $entityManager->remove($entity);
+            }
+            $entityManager->flush();
 
-        $entityManager->remove($wardrobe);
-        $entityManager->flush();
+            return $this->json(["Done"], 202, ["Access-Control-Allow-Origin" => "*", "Access-Control-Allow-Headers" => "*"], ["groups"=>["show_wardrobe"]]);
+        }
+        else{
+            $wardrobe = $this->getDoctrine()->getRepository(Wardrobe::class)->find($id);
+
         
-        return $this->json($wardrobe, 202, ["Access-Control-Allow-Origin" => "*", "Access-Control-Allow-Headers" => "*"], ["groups"=>["show_wardrobe"]]);
+            if ($wardrobe == null){
+                return $this->json([
+                    "error"=>"Wardrobe not found"
+                    ], 400, ["Access-Control-Allow-Origin" => "*", "Access-Control-Allow-Headers" => "*"], []);
+            }
+
+            $entityManager->remove($wardrobe);
+            $entityManager->flush();
+        
+            return $this->json($wardrobe, 202, ["Access-Control-Allow-Origin" => "*", "Access-Control-Allow-Headers" => "*"], ["groups"=>["show_wardrobe"]]);
+        }
     }
 }
 ?>
